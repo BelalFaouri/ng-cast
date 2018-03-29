9d9e8c375fb467d27d2d8c66888f08a1ae54db3e
Belalfaouri <belalfaourig@gmail.com>
2018-03-29 16:25:24 +0200

    displayed views

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,5 +1,26 @@
-angular.module('video-player')
+var vp=angular.module('video-player')
 
-.component('videoPlayer', {
-  // TODO
+vp.component('videoPlayer', {
+  bindings:{
+    videos: '<'
+  },
+  controller:function(){
+    console.log(this)
+  },
+
+  template:  `
+  <div class="video-player">
+  <div>{{$ctrl.videos.id.videoId}}</div>
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe class="embed-responsive-item" ng-src="{{'https://www.youtube.com/embed/'+$ctrl.videos.id.videoId}}" allowFullScreen>asd</iframe>
+    </div>
+    <h3>{{$ctrl.videos.snippet.title}}</h3>
+
+    <div class="video-player-details">
+      <div>{{$ctrl.videos.snippet.description}}</div>
+    </div>
+  </div>
+`
 });
+
+console.log(vp.$ctrl.videos);
